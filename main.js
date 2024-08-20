443e3ea8c26cfcf26523dbd27b64a4f8ae73600f
peter.liao <pidder.liao@gmail.com>
2024-08-20 10:00:21 +0800

    ✨feature : add Env

---
+++ main.js
@@ -0,0 +1,10 @@
+import './src/styles/globals.scss'
+import './src/styles/custom.scss'
+fetch('@assets/sprite.svg')
+.then(response => response.text())
+.then(data => {
+    const div = document.createElement('div');
+    div.style.display = 'none';
+    div.innerHTML = data;
+    document.body.insertBefore(div, document.body.childNodes[0]);
+});
